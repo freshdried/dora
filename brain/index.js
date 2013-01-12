143078ce5497b0c603f31ee19867c451616b2352
Sean Lee <freshdried@gmail.com>
2013-01-12 13:18:03 -0500

    lol

--- brain/index.js
+++ brain/index.js
@@ -35,20 +35,20 @@ var REMOTE_PHILIPS_RC_5331 = {
 };
 var remote = REMOTE_PHILIPS_RC_5331;
 
-var handle = function(){
-	this.press =  {};
-	press[remote.POWER] = process.exit;
+var handle = {};
 
-	press[remote.OK] = relays.toggleAll;
-	press[remote.ONE] = relays.toggleA;
-	press[remote.TWO] = relays.toggleB;
-	press[remote.THREE] = relays.toggleC;
+handle.press = {};
+handle.press[remote.POWER] = process.exit;
 
-	press[remote.PLAY] = function(write){
-		spawn('mpg123', ['wakeup.mp3']);
-	}
+handle.press[remote.OK] = relays.toggleAll;
+handle.press[remote.ONE] = relays.toggleA;
+handle.press[remote.TWO] = relays.toggleB;
+handle.press[remote.THREE] = relays.toggleC;
 
-	this.hold={};
-};
+handle.press[remote.PLAY] = function(write){
+	spawn('mpg123', ['wakeup.mp3']);
+}
+
+handle.holdi = {};
 
 ss.start(handle);
