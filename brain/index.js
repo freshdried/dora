fe58a79ff729d7c84f505a1e7fe0f770d1428d3c
Sean Lee <freshdried@gmail.com>
2013-01-12 01:05:09 -0500

    yeah

--- brain/index.js
+++ brain/index.js
@@ -39,7 +39,7 @@ handle[remote.ONE] = relays.toggleA;
 handle[remote.TWO] = relays.toggleB;
 handle[remote.THREE] = relays.toggleC;
 handle[remote.SOUTH] = function(write){
-	for(int i = 0; i< 100; i ++){
+	for(var i = 0; i< 100; i ++){
 		relays.toggleC(write);
 	};
 };
