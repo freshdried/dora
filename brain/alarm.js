f93a0ba01b3d8bd92cad7429e328b196b79aa463
Sean Lee <freshdried@gmail.com>
2013-01-16 10:45:20 -0500

    lol

--- brain/alarm.js
+++ brain/alarm.js
@@ -1,9 +1,7 @@
-var EventEmitter = require('events').eventemitter;
+var EventEmitter = require('events').EventEmitter;
 var util = require('util');
 
 //TODO: figure out how Date module works...
-// Also currently my alarm is really a timer.
-// Make it a
 
 var timer = function(time, callback){
 	var self = this;
