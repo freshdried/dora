db42e307fa4b2882f863fa61d64f8043ea124933
Sean Lee <freshdried@gmail.com>
2013-04-21 23:59:00 -0400

    lol

--- brain/index.js
+++ brain/index.js
@@ -3,6 +3,7 @@ var cronjob = require("cron").CronJob;
 var relays = require("./relays");
 var spawn = require('child_process').spawn;
 var exec = require('child_process').exec;
+var request = require('request');
 
 
 var remote = require("./remotes/PHILIPS_RC_5331.js");
@@ -29,6 +30,17 @@ var remotehandle = {
 			str = str.split('')[3] == '0' ? str.substring(0,3) + 'o' + str.substring(4) : str;
 			exec("echo it is " + str + "|espeak --stdout -s 120| aplay", 
 			     function(error, stdout, stderr){
+			});
+		};
+		this[remote.SWEST] = function(){
+			request('http://data.mtgox.com/api/1/BTCUSD/ticker', function(error, response, body){
+				if(!error && response.statusCode == 200){
+					var last = JSON.parse(body)['return']['last']['display_short'].toString().substring(1);
+					str = last + ' dollars';
+					exec(str + "espeak --stdout -s 120| aplay", 
+					     function(error, stdout, stderr){
+					});
+				}
 			});
 		};
 	},
