cb6e71d4406bcd2b444846504f33f91d19c04cbc
Sean Lee <freshdried@gmail.com>
2013-06-18 23:52:46 -0400

    created sensory.js

--- sensory.js/sensory.js
+++ sensory.js/sensory.js
@@ -1,84 +1,32 @@
-var ss = require("./serialserver");
-var cronjob = require("cron").CronJob;
-var relays = require("./relays");
-var spawn = require('child_process').spawn;
-var exec = require('child_process').exec;
-var request = require('request');
+var serialport = require("serialport");
+
+var sp = new serialport.SerialPort("/dev/ttyACM0",{
+	parser: serialport.parsers.readline("\n"),
+	baudrate: 9600
+});
 
 
-var remote = require("./remotes/PHILIPS_RC_5331.js");
-
-var remotehandle = { 
-	//Handler object
-	//
-	//Contains press and hold objects, which each contain
-	//  functions for each button
-	//
-	//press takes precedence over hold:
-	//If press.BUTTON exists, then hold.BUTTON will be ignored
-	press: new function(){
-		this[remote.POWER] = process.exit;
-
-		this[remote.OK] = relays.toggleAll;
-		this[remote.ONE] = relays.toggleA;
-		this[remote.TWO] = relays.toggleB;
-		this[remote.THREE] = relays.toggleC;
-
-		//this[remote.PLAY] = function(){
-		//	spawn('mpg123', ['sounds/siren_alan.mp3']);
-		//};
-		//this[remote.MUTE] = function(){
-		//	spawn('amixer', ['set', 'PCM', 'togglemute']);
-		//};
-		
-	        // Say current time
-		this[remote.SOUTH] = function(){
-			var date = new Date()
-			var hours = (parseInt(date.getHours()) % 12).toString()
-			hours = hours != "0" ? hours : "12";
-
-			var minutes = date.getMinutes().toString();
-			if (minutes == '0'){
-				minutes = 'o clock';
-			}else if (minutes.length == 1){
-				minutes = "o" + minutes;
-			}
-			exec("echo it is " + hours + ' ' + minutes + "|espeak --stdout| aplay", 
-			     function(error, stdout, stderr){
-			});
+sp.on("open", function(){
+	var lastraw; //buffer to holder the last signaled button code
+	sp.on('data', function(data){
+		/*
+		 * PHILIPS_RC-5331 functionality:
+		 *
+		 * Each time a button is released, the (16^5)th place of the next ir message is flipped. Aside from that digit, the remote fires a distinct message for each button. When a button is pressed, the distinct message (plus the flipping digit) is fired; if the button is held down, every 100ms after it was first pressed, the remote fires the same message until the button is released.
+		 *
+		 *
+		 */
+		var raw = parseInt(data, 16);
+		var code = raw % 0x10000;
+		if(raw != lastraw){
+			console.log(code);
 		};
-		// Say last bitcoin price in USD on MtGox
-		this[remote.SWEST] = function(){
-			request('http://data.mtgox.com/api/1/BTCUSD/ticker', function(error, response, body){
-				if(!error && response.statusCode == 200){
-					var last = JSON.parse(body)['return']['last_local']['display_short'].toString().substring(1);
-					str = last + ' dollars';
-					exec('echo ' + str + "|espeak --stdout -s 180| aplay", 
-					     function(error, stdout, stderr){
-					});
-				}
-			});
-		};
-	},
-	hold: new function(){
-		//this[remote.UP] = function(){
-		//	spawn('amixer', ['set', 'PCM', '5%+', '-M']);
-		//};
-		//this[remote.DOWN] = function(){
-		//	spawn('amixer', ['set', 'PCM', '5%-', '-M']);
-		//};
-	},
-};
+		//on hold
+		lastraw = raw;
+	});
+});
 
-var cronjobs = function(){
-	//new cronjob('00 00 06 * * 1-5', function(){
-	//	spawn('mpg123', ['sounds/wake_alan.mp3']);
-	//},null, true);
 
-	//new cronjob('00 30 06 * * 1-5', function(){
-	//	spawn('mpg123', ['sounds/wake_alan.mp3']);
-	//},null, true);
-}
-
-ss.start(remotehandle);
-new cronjobs();
+sp.on("close", function(){
+	console.log('close');
+});
